579a830e20befe10e9cdb7814e8e44268b2a227f
travis ouellette <travis.brouellette@gmail.com>
2018-11-07 17:00:28 -0800

    Initial Commit
    Nothing special, just a barebones collection of how the network might look

---
+++ Backpropagation.m
@@ -0,0 +1,10 @@
+function [] = Backpropagation()
+    % Welcome the inputs to the network
+    
+    % Read more about what this network needs to do, then fill in the
+    % pieces here
+    
+    
+    disp("Start this network by passing in some inputs!")
+end
+
---
+++ GettingStarted.m
@@ -0,0 +1,17 @@
+% Introduce the data file from MNIST
+x = 0:pi/100:2*pi;
+y = sin(x);
+
+plot(x,y)
+
+% Break into subgroups of around 12 elements?
+
+
+% Loop through the subgroup and pass each element to the backprop network
+Backpropagation()
+
+% Handle the results
+
+
+% Create graphical representations of the inputs and outputs
+
