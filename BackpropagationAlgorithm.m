49d78351323e89eaead85d5675460287084d3d4c
travis ouellette <travis.brouellette@gmail.com>
2018-11-13 18:20:24 -0800

    WERE GETTING ACTIVATIONS NOW BOI

--- BackpropagationAlgorithm.m
+++ BackpropagationAlgorithm.m
@@ -2,15 +2,15 @@
     % Create an array to hold the output of each neuron in this layer
     neuronActivations = zeros(size(B, 1), 1);
  
-    % Compute the forward pass through the network
+    % Compute the neuron activations for the second layer
     for neuronIndex = 1:size(neuronActivations, 1)
         subsetStartIndex = (1 + ((neuronIndex*784) - 784));
         subsetEndIndex = (neuronIndex*784);
         subset = W([subsetStartIndex:subsetEndIndex], :);
-        neuronActivations(neuronIndex) = (P'*subset) + B(neuronIndex); % W*p + b
+        a = LogSigmoid((P'*subset) + B(neuronIndex));
+        neuronActivations(neuronIndex) = a;
         subset = [];
     end
-    %a = ComputeForwardPass(input, weightMatrix, biases);
     
     % Compute the error: e = (t - a)
     
